873bd09b14b71f64b0ac62108b31464fea2e91de
CalendulaED <edmundwuyuxuan@gmail.com>
2022-09-27 11:23:21 -0400

    initial commit

---
+++ src/columbia_student_resource.py
@@ -0,0 +1,42 @@
+import pymysql
+
+import os
+
+class ColumbiaStudentResource:
+
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def _get_connection():
+
+
+        usr = os.environ.get("DBUSER")
+        pw = os.environ.get("DBPW")
+        h = os.environ.get("DBHOST")
+        #
+        # print(usr)
+        # print(pw)
+        # print(h)
+
+        conn = pymysql.connect(
+
+            user=usr,
+            password=pw,
+            host=h,
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+        return conn
+
+    @staticmethod
+    def get_by_key(key):
+
+        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
+        conn = ColumbiaStudentResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=key)
+        result = cur.fetchone()
+
+        return result
+
